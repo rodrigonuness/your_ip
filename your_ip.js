858e3d93934f0eb38b0dac88877c85361df7df5d
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-09-09 08:58:04 -0300

    Create your_ip.js

---
+++ your_ip.js
@@ -0,0 +1,12 @@
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+    var ip = add
+    console.log(ip)
+const fs = require("fs");
+fs.writeFile(`./ip.js`, `var ip = "${ip}";\n`,
+        function (erro) {
+            if (erro) {
+                throw erro;
+            }
+            console.log("File saved successfully!!");
+        });
+    })
